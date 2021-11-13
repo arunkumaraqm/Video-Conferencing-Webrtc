ee12c5ce525aa83ab998b96e87f7f7b1a1913d81
AkhilT-Sam <70057670+AkhilT-Sam@users.noreply.github.com>
2021-11-13 19:40:31 +0530

    video change

--- src/RoomPage/VideoSection/VideoSection.js
+++ src/RoomPage/VideoSection/VideoSection.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import VideoButtons from "./VideoButtons";
 import Videos from "./Videos";
+import video1 from "../../resources/images/Stencil.mp4"
 
 const VideoSection = () => {
   const [room, setRoom] = useState(null);
@@ -8,6 +9,7 @@ const VideoSection = () => {
   return (
     <div className="video_section_container">
       <Videos room={room} setRoom={setRoom} />
+      <video src={video1} width="600" height="300" controls="true" playsInline="true" autoPlay="true" />
       <VideoButtons room={room} />
     </div>
   );
